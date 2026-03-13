288e559f258349f94522e1055dfec00cc4e836ab
Kaan Borucu <furkankborucu@gmail.com>
2026-03-13 11:00:55 +0300

    update

--- build.sh
+++ build.sh
@@ -1,17 +1,46 @@
 #!/usr/bin/env bash
-
 set -e
 
 echo "====================================="
 echo "OCS2 Quadruped Docker Build Script"
 echo "====================================="
 
-# allow docker GUI access
-echo "[1/4] Enabling X11 access for Docker..."
-xhost +local:docker > /dev/null
+WS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+SRC_DIR="$WS_DIR/src/Quadruped-Control-OCS2-ROS2"
 
-# build docker image
-echo "[2/4] Building Docker image..."
+echo "[1/4] Enable X11"
+xhost +local:docker > /dev/null 2>&1 || true
+
+echo "[2/4] Checking external dependencies..."
+
+cd "$SRC_DIR"
+
+clone_if_missing () {
+if [ ! -d "$1/.git" ]; then
+echo "Cloning $1..."
+git clone "$2"
+else
+echo "$1 already exists"
+fi
+}
+
+clone_if_missing "ocs2_ros2" https://github.com/zhengxiang94/ocs2_ros2.git
+clone_if_missing "ocs2_robotic_assets" https://github.com/zhengxiang94/ocs2_robotic_assets.git
+clone_if_missing "plane_segmentation_ros2" https://github.com/zhengxiang94/plane_segmentation_ros2.git
+
+if [ ! -d pinocchio/.git ]; then
+echo "Cloning pinocchio..."
+git clone --recurse-submodules https://github.com/zhengxiang94/pinocchio.git
+fi
+
+if [ ! -d hpp-fcl/.git ]; then
+echo "Cloning hpp-fcl..."
+git clone --recurse-submodules https://github.com/zhengxiang94/hpp-fcl.git
+fi
+
+cd "$WS_DIR"
+
+echo "[3/4] Building Docker image"
 docker compose build
 
 MUJOCO_LIB="src/Quadruped-Control-OCS2-ROS2/mujoco/mujoco-3.2.2/lib"
@@ -24,14 +53,37 @@ fi
 # build ROS workspace inside container
 echo "[3/4] Building ROS2 workspace..."
 
-docker compose run --rm quad_ocs2 bash -c "
-colcon build \
-  --packages-up-to motion_control mujoco_simulator user_command launch_simulation \
-  --symlink-install \
-  --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+docker compose run --rm 
+-u $(id -u):$(id -g) 
+-v "$WS_DIR:/workspaces/quad_ocs2_ws" 
+quad_ocs2 bash -c "
+
+set -e
+source /opt/ros/humble/setup.bash
+cd /workspaces/quad_ocs2_ws
+
+echo 'Cleaning previous build'
+rm -rf build install log || true
+
+echo 'Building OCS2 core packages'
+colcon build 
+--packages-up-to ocs2_legged_robot_ros ocs2_self_collision_visualization 
+--cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+echo 'Sourcing workspace'
+source install/setup.bash
+
+echo 'Building simulator packages'
+colcon build --packages-up-to mujoco_simulator
+colcon build --packages-up-to motion_control
+colcon build --packages-up-to user_command
+colcon build --packages-up-to launch_simulation
 "
 
-echo "[4/4] Build finished successfully!"
 echo ""
-echo "To start simulation run:"
+echo "====================================="
+echo "Build finished successfully"
+echo "====================================="
+echo ""
+echo "Run the simulator with:"
 echo "./run.sh"
